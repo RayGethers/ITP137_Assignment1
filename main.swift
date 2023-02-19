22d1486a7f7100525c1e32943ecc5ef3d32bfc2b
RayGethers <124451441+RayGethers@users.noreply.github.com>
2023-02-19 07:43:29 +0000

    uncommented libraries

--- main.swift
+++ main.swift
@@ -1,5 +1,5 @@
-// import Foundation
-// import Glibc
+import Foundation
+import Glibc
 
 // // QUADRATIC EQUATION
 // // --------------------------------------
@@ -41,17 +41,25 @@
 // PIGGY BANK
 // --------------------------------------
 
-// 0.054 - Pesos
-// 0.19 - Reals
-// 0.26 - Soles
+// Constants for conversion rates
+let p: Double = 0.054 // - Pesos
+let r: Double = 0.19 // - Reals
+let s: Double = 0.26 // - Soles
+
+// Variables
+// for user input
 var pesos: Double
 var reals: Double
 var soles: Double
+// to store converted value
 var usd1: Double
 var usd2: Double
 var usd3: Double
+// total of all conversions in USD
 var total: Double
 
+// Ask the user to input how much of 
+// each currency they acquired from each country
 print("Enter Peso amount:")
 pesos = Double(readLine()!)!
 print("Enter Real amount:")
@@ -59,10 +67,12 @@ reals = Double(readLine()!)!
 print("Enter Sole amount:")
 soles = Double(readLine()!)!
 
+// Converting each currency to USD
 usd1 = (pesos * 0.054)
 usd2 = (reals * 0.19)
 usd3 = (soles * 0.26)
 
+// Printing conversions
 print(" ")
 print("Conversions")
 print("- - - - - - -")
@@ -71,5 +81,7 @@ print("\(reals) -> $\(usd2)")
 print("\(soles) -> $\(usd3)")
 print("- - - - - - -")
 
+// Adding each conversion to get a total in USD
+// And printing the total out 
 total = usd1 + usd2 + usd3
 print("Total comes out to $\(total)")
